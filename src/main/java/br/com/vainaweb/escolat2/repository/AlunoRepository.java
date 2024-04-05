1206fd69c2f94840e3e302f8fcfa861065c5635d
Marlonsouto <soutomarlon@gmail.com>
2024-04-05 13:19:06 -0300

    feat:(AlunoService): Adicionando nova funcionalidade ListarAlunos

--- src/main/java/br/com/vainaweb/escolat2/repository/AlunoRepository.java
+++ src/main/java/br/com/vainaweb/escolat2/repository/AlunoRepository.java
@@ -3,6 +3,9 @@ package br.com.vainaweb.escolat2.repository;
 import br.com.vainaweb.escolat2.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
 
+    Optional<Aluno> findAllById(int id);
 }
