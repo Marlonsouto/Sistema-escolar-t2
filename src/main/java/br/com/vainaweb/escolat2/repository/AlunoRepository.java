abaee5b41e419b728cfd142d0e1d723ecab2a7ba
Marlonsouto <soutomarlon@gmail.com>
2024-04-08 19:18:43 -0300

    fench(AlunoRepository): Criando busca no db personalizada

--- src/main/java/br/com/vainaweb/escolat2/repository/AlunoRepository.java
+++ src/main/java/br/com/vainaweb/escolat2/repository/AlunoRepository.java
@@ -2,12 +2,18 @@ package br.com.vainaweb.escolat2.repository;
 
 import br.com.vainaweb.escolat2.model.Aluno;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
 @Repository
 public interface AlunoRepository extends JpaRepository<Aluno, Long> {
-
     Optional<Aluno> findAllById(int id);
+
+    @Query("nome,email,curso from tb_alunos aluno Where aluno.id =: id ")
+    Optional<Set<Aluno>> ListaDeAluno (Long Id);
 }
